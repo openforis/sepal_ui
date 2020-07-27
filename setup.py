c858534b9e46559dce265a3b5a499749a2bed5db
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-07-27 09:37:37 +0000

    remove long description

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 from distutils.core import setup
-
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
-    long_description = f.read()
     
 setup(
     name = 'sepal_ui',      
     packages = ['sepal_ui'],   
-    version = '0.0',   
+    version = '0.1.2-alpha',   
     license='MIT',        
     description = 'wrapper for ipyvuetify widgets to unify the display of voila dashboards in the sepal plateform',  
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     author = 'Pierrick Rambaud',                   
     author_email = 'pierrick.rambaud49@gmail.com',  
     url = 'https://github.com/12rambau/sepal_ui',
-    download_url = 'https://github.com/12rambau/sepal_ui/archive/v_0.1-alpha.tar.gz',
+    download_url = 'https://github.com/12rambau/sepal_ui/archive/v_0.1.2-alpha.tar.gz',
     keywords = ['UI', 'Python', 'widget', 'sepal'], 
     install_requires=[
         'haversine',
