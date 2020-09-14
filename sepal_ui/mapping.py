f78fb2fb421cd0be36c899629999c815804f43f9
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-09-14 12:30:26 +0000

    V0.6

--- sepal_ui/mapping.py
+++ sepal_ui/mapping.py
@@ -144,7 +144,7 @@ class SepalMap(geemap.Map):
         """Adds a local raster dataset to the map.
         Args:
             image (str): The image file path.
-            bands (int or list, optional): The image bands to use. It can be either a nubmer (e.g., 1) or a list (e.g., [3, 2, 1]). Defaults to None.
+            bands (int or list, optional): The image bands to use. It can be either a number (e.g., 1) or a list (e.g., [3, 2, 1]). Defaults to None.
             layer_name (str, optional): The layer name to use for the raster. Defaults to None.
             colormap (str, optional): The name of the colormap to use for the raster, such as 'gray' and 'terrain'. More can be found at https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html. Defaults to None.
             x_dim (str, optional): The x dimension. Defaults to 'x'.
