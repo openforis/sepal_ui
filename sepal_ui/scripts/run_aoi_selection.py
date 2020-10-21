665ec4da366fd5750880e8839b0c7df589910a90
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-10-21 10:31:44 +0000

    reduce complexity of run_aoi_selection Fix #40

--- sepal_ui/scripts/run_aoi_selection.py
+++ sepal_ui/scripts/run_aoi_selection.py
@@ -28,8 +28,117 @@ def isAsset(asset_descripsion, folder):
         if asset['name'] == folder + asset_descripsion:
             exist = True
             break
-    return exist    
+    return exist 
 
+def get_country_asset(country_selection, folder, output):
+    """send a request to GEE to create an asset based on the country name"""
+    
+    if country_selection == None:
+        output.add_msg(ms.NO_COUNTRY, 'warning')
+        return None
+    
+    country_code = utils.create_FIPS_dic()[country_selection] 
+    asset_descripsion = ms.FILE_PATTERN.format(country_code) 
+    asset = folder + asset_descripsion
+            
+    #check asset existence
+    if isAsset(asset_descripsion, folder):
+        output.add_msg(ms.ASSET_ALREADY_EXIST.format(asset), 'success')
+    else:  
+        country = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017').filter(ee.Filter.eq('country_co', country_code))
+                
+        #create and launch the task
+        task_config = {
+            'collection': country, 
+            'description':asset_descripsion,
+            'assetId': asset
+        }
+        task = ee.batch.Export.table.toAsset(**task_config)
+        task.start()
+        gee.wait_for_completion(asset_descripsion, output)
+          
+        output.add_msg(ms.ASSET_CREATED.format(asset), 'success')
+    
+    return asset
+
+def get_drawn_shape(drawn_feat, file_name, folder, output):
+    """send the requested drawn feature to GEE to create an asset"""
+    
+    aoi = drawn_feat 
+    asset_name = ms.FILE_PATTERN.format(re.sub('[^a-zA-Z\d\-\_]', '_', file_name))
+        
+    if drawn_feat == None: #check if something is drawn 
+        output.add_msg(ms.NO_SHAPE, 'error')
+        return None
+    elif isAsset(asset_name, folder): #check asset existence
+        output.add_msg(ms.NAME_USED, 'error')
+        return None 
+        
+    asset = folder + asset_name
+            
+    #create and launch the task
+    task_config = {
+        'collection': drawn_feat, 
+        'description':asset_name,
+        'assetId': asset
+    }
+    task = ee.batch.Export.table.toAsset(**task_config)
+    task.start()
+    gee.wait_for_completion(asset_name, output)
+           
+    output.add_msg(ms.ASSET_CREATED.format(asset), 'success') 
+    
+    return asset
+
+def get_gee_asset(asset_name, output):
+    """check that the asset exist return None if not"""
+    
+    if asset_name == '' or asset_name == None:
+        output.add_msg(ms.NO_ASSET, 'error') 
+        asset = None
+    else:
+        output.add_msg(ms.CHECK_IF_ASSET, 'success')
+        asset = asset_name
+        
+    return asset
+            
+def get_shp_aoi(file_input, folder, output):
+    """send a request to GEE to create an asset based on the local shapefile"""
+    
+    if not os.path.isfile(file_input):
+        output.add_msg(ms.ERROR_OCCURED, 'error')
+        return None
+        
+    ee_object = geemap.shp_to_ee(file_input)           
+        
+    name = os.path.split(file_input)[1]
+        
+    asset_name = ms.FILE_PATTERN.format(re.sub('[^a-zA-Z\d\-\_]','_',name))
+        
+    #check asset's name
+    if isAsset(asset_name, folder):
+        
+        output.add_msg(ms.NAME_USED, 'error')
+        asset = None
+        
+    else:
+        
+        asset = folder + asset_name
+            
+        #launch the task
+        task_config = {
+            'collection': ee_object, 
+            'description':asset_name,
+            'assetId': asset
+        }
+        task = ee.batch.Export.table.toAsset(**task_config)
+        task.start()
+        gee.wait_for_completion(asset_name, output)
+                   
+        output.add_msg(ms.ASSET_CREATED.format(asset), 'success')
+    
+    return asset
+    
 def run_aoi_selection(file_input, file_name, country_selection, asset_name, drawing_method, output, list_method, drawn_feat):
     """ create an gee asset according to the user inputs
 
@@ -55,103 +164,15 @@ def run_aoi_selection(file_input, file_name, country_selection, asset_name, draw
         asset = None
         
     elif drawing_method == list_method[0]: #use a country boundary
-        if country_selection == None:
-            output.add_msg(ms.NO_COUNTRY, 'warning') 
-            asset = None
-            return asset
-        country_code = utils.create_FIPS_dic()[country_selection] 
-        asset_descripsion = ms.FILE_PATTERN.format(country_code) 
-        asset = folder + asset_descripsion
-            
-        #check asset existence
-        if isAsset(asset_descripsion, folder):
-            output.add_msg(ms.ASSET_ALREADY_EXIST.format(asset), 'success')
-            return asset
-        
-        country = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017').filter(ee.Filter.eq('country_co', country_code))
-                
-        #create and launch the task
-        task_config = {
-            'collection': country, 
-            'description':asset_descripsion,
-            'assetId': asset
-        }
-        task = ee.batch.Export.table.toAsset(**task_config)
-        task.start()
-        gee.wait_for_completion(asset_descripsion, output)
-          
-        output.add_msg(ms.ASSET_CREATED.format(asset), 'success')
-                
-    elif drawing_method == list_method[1]: #draw a shape
+        asset = get_country_asset(country_selection, folder, output)
              
-        aoi = drawn_feat 
-        asset_name = ms.FILE_PATTERN.format(re.sub('[^a-zA-Z\d\-\_]', '_', file_name))
+    elif drawing_method == list_method[1]: #draw a shape
+        asset = get_drawn_shape(drawn_feat, file_name, folder, output)
         
-        #check if something is drawn 
-        if drawn_feat == None:
-            asset = None
-            output.add_msg(ms.NO_SHAPE, 'error')
-            return asset
-              
-        #check asset existence
-        if isAsset(asset_name, folder):
-            asset = None
-            output.add_msg(ms.NAME_USED, 'error')
-            return asset 
-        
-        asset = folder + asset_name
-            
-        #create and launch the task
-        task_config = {
-            'collection': aoi, 
-            'description':asset_name,
-            'assetId': asset
-        }
-        task = ee.batch.Export.table.toAsset(**task_config)
-        task.start()
-        gee.wait_for_completion(asset_name, output)
-           
-        output.add_msg(ms.ASSET_CREATED.format(asset), 'success')           
-            
     elif drawing_method == list_method[3]: #use GEE asset
-        
-        #verify that there is an asset
-        if asset_name == '' or asset_name == None:
-            asset = None
-            output.add_msg(ms.NO_ASSET, 'error') 
-        else:
-            asset = asset_name
-            output.add_msg(ms.CHECK_IF_ASSET, 'info')
+        asset = get_gee_asset(asset_name, output)
             
     elif drawing_method == list_method[2]: #upload file
-        if not os.path.isfile(file_input):
-            output.add_msg(ms.ERROR_OCCURED, 'error')
-            asset = None
-            return asset
-        
-        ee_object = geemap.shp_to_ee(file_input)           
-        
-        name = os.path.split(file_input)[1]
-        
-        asset_name = ms.FILE_PATTERN.format(re.sub('[^a-zA-Z\d\-\_]','_',name))
-        
-        #check asset's name
-        if isAsset(asset_name, folder):
-            asset = None
-            output.add_msg(ms.NAME_USED, 'error')
-        else:
-            asset = folder + asset_name
-            
-            #launch the task
-            task_config = {
-                'collection': ee_object, 
-                'description':asset_name,
-                'assetId': asset
-            }
-            task = ee.batch.Export.table.toAsset(**task_config)
-            task.start()
-            gee.wait_for_completion(asset_name, output)
-                   
-            output.add_msg(ms.ASSET_CREATED.format(asset), 'success')
-            
+        asset = get_shp_aoi(file_input, folder, output)
+         
     return asset
