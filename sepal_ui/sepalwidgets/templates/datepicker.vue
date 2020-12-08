61c2b8f2977dc5049bd168fa7d6afdfb9d79b9f2
ingdanielguerrero <dfgm2006@gmail.com>
2020-12-08 13:43:40 +0000

    templates based on vue

---
+++ sepal_ui/sepalwidgets/templates/datepicker.vue
@@ -0,0 +1,28 @@
+<v-menu
+    v-model="menu"
+    :close-on-content-click="false"
+    :nudge-right="40"
+    transition="scale-transition"
+    offset-y
+    min-width="290px"
+    dark
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        v-model="v_model"
+        :label="label"
+        prepend-icon="mdi-calendar"
+        readonly
+        v-bind="attrs"
+        v-on="on"          >
+      </v-text-field>
+    </template>
+    <v-date-picker
+
+      v-model="v_model"
+      dark
+      @input="menu = false"
+      @input=date_click()
+    >
+    </v-date-picker>
+</v-menu>
