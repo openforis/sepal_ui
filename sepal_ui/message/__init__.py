f7e93266b1366ad8eccfdda6d9a8c516c4e3be6b
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-01-21 15:43:37 +0000

    typo in the translator declaration

--- sepal_ui/message/__init__.py
+++ sepal_ui/message/__init__.py
@@ -8,4 +8,4 @@ lang = 'en'
 if 'CUSTOM_LANGUAGE' in os.environ:
     lang = os.environ['CUSTOM_LANGUAGE']
 
-ms = Translator(Path(__file__).parent, 'en', lang)
+ms = Translator(Path(__file__).parent, lang)
