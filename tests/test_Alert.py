541d662f8bf278007664d414eab950939b827475
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-01-28 10:28:57 +0000

    test binding verbose

--- tests/test_Alert.py
+++ tests/test_Alert.py
@@ -8,15 +8,15 @@ class TestAlert(unittest.TestCase):
     
     def test_init(self):
         alert = sw.Alert()
-        self.assertFalse(alert.viz, 'the alert is set to viz')
-        self.assertEqual(alert.type, 'info', 'the alert is not set to "info"')
+        self.assertFalse(alert.viz)
+        self.assertEqual(alert.type, 'info')
         
         for type_ in sw.TYPES:
             alert = sw.Alert(type_)
-            self.assertEqual(alert.type, type_, 'the type_ {} cannot be set'.format(type_))
+            self.assertEqual(alert.type, type_)
             
         alert = sw.Alert('random')
-        self.assertEqual(alert.type, 'info', "the alert didn't fallback to 'info'")
+        self.assertEqual(alert.type, 'info')
         
         return
         
@@ -27,19 +27,19 @@ class TestAlert(unittest.TestCase):
         #single msg
         res = alert.add_msg(msg)
         self.assertEqual(res, alert)
-        self.assertTrue(alert.viz, 'the alert is not displayed')
-        self.assertEqual(alert.children[0].children[0], msg, 'the message is not diplayed')
+        self.assertTrue(alert.viz)
+        self.assertEqual(alert.children[0].children[0], msg)
         
         #single msg with type 
         for type_ in sw.TYPES:
             alert.add_msg(msg, type_)
-            self.assertEqual(alert.type, type_, 'the type_ {} cannot be set'.format(type_))
-            self.assertEqual(alert.children[0].children[0], msg, 'the message is not diplayed')
+            self.assertEqual(alert.type, type_)
+            self.assertEqual(alert.children[0].children[0], msg)
             
         #single msg with rdm type
         alert.add_msg(msg, 'random')
-        self.assertEqual(alert.type, 'info', "the alert didn't fallback to 'info'")
-        self.assertEqual(alert.children[0].children[0], msg, 'the message is not diplayed')
+        self.assertEqual(alert.type, 'info')
+        self.assertEqual(alert.children[0].children[0], msg)
         
         return
         
@@ -50,19 +50,19 @@ class TestAlert(unittest.TestCase):
         #single msg
         res = alert.add_live_msg(msg)
         self.assertEqual(res, alert)
-        self.assertTrue(alert.viz, 'the alert is not displayed')
-        self.assertEqual(alert.children[1].children[0], msg, 'the message is not diplayed')
+        self.assertTrue(alert.viz)
+        self.assertEqual(alert.children[1].children[0], msg)
         
         #single msg with type 
         for type_ in sw.TYPES:
             alert.add_live_msg(msg, type_)
-            self.assertEqual(alert.type, type_, 'the type_ {} cannot be set'.format(type_))
-            self.assertEqual(alert.children[1].children[0], msg, 'the message is not diplayed')
+            self.assertEqual(alert.type, type_)
+            self.assertEqual(alert.children[1].children[0], msg)
             
         #single msg with rdm type
         alert.add_live_msg(msg, 'random')
-        self.assertEqual(alert.type, 'info', "the alert didn't fallback to 'info'")
-        self.assertEqual(alert.children[1].children[0], msg, 'the message is not diplayed')
+        self.assertEqual(alert.type, 'info')
+        self.assertEqual(alert.children[1].children[0], msg)
         
         return
     
@@ -115,23 +115,26 @@ class TestAlert(unittest.TestCase):
         widget = v.TextField(v_model=None)
         alert = sw.Alert()
         alert2 = sw.Alert()
+        alert3 = sw.Alert()
         
-        #binding without text 
+        # binding without text 
         res = alert.bind(widget, test_io, 'out')
         alert2.bind(widget, test_io, 'out', 'new variable : ')
+        alert3.bind(widget, test_io, 'out', verbose=False)
         
         self.assertEqual(res, alert)
         
         
-        #check when value change
+        # check when value change
         msg = 'toto'
         widget.v_model = msg
         
-        self.assertTrue(alert.viz, 'the alert remained hidden')
-        self.assertEqual(test_io.out, widget.v_model, "the io didn't change")
+        self.assertTrue(alert.viz)
+        self.assertEqual(test_io.out, widget.v_model)
         
-        self.assertEqual(alert.children[0].children[0], 'The selected variable is: {}'.format(msg), 'the variable is not diplayed')
-        self.assertEqual(alert2.children[0].children[0], 'new variable : {}'.format(msg), 'display does not use custom msg')
+        self.assertEqual(alert.children[0].children[0], 'The selected variable is: {}'.format(msg))
+        self.assertEqual(alert2.children[0].children[0], 'new variable : {}'.format(msg))
+        self.assertFalse(len(alert3.children[0]))
         
         return 
     
@@ -141,9 +144,9 @@ class TestAlert(unittest.TestCase):
 
         var_test = None
         res = alert.check_input(var_test)
-        self.assertFalse(res, 'not returning accurate value')
+        self.assertFalse(res)
         self.assertTrue(alert.viz)
-        self.assertEqual(alert.children[0].children[0], "The value has not been initialized")
+        self.assertEqual(alert.children[0].children[0])
         
         res = alert.check_input(var_test, 'toto')
         self.assertEqual(alert.children[0].children[0], 'toto')
