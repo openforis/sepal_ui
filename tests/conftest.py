5c1f68b6e139cfb8737760f839644d8daf830a43
dfguerrerom <dfgm2006@gmail.com>
2021-09-30 12:56:56 +0000

    feat: create useful fixtures to implement in related tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import geopandas as gpd
 import pytest
 from pathlib import Path
 from zipfile import ZipFile
@@ -10,6 +11,7 @@ from sepal_ui.aoi import AoiModel
 @pytest.fixture
 def dum_dir():
     """Creates a dummy directory"""
+
     dum_dir = Path("~").expanduser() / "tmp/sepal_ui_tests/"
     dum_dir.mkdir(exist_ok=True, parents=True)
 
@@ -26,22 +28,16 @@ def aoi_model_gee(dum_dir):
 
 
 @pytest.fixture
-def aoi_model_no_gee(dum_dir):
+def aoi_model_local(dum_dir):
     """Creates a default AOI NOT-GEE model with a default vector"""
 
-    name = "gadm36_VAT_0"
-    file = dum_dir / f"{name}.zip"
-    vector = file.with_suffix(".shp")
+    # Get a dummy asset
+    file = Path(gpd.datasets.get_path("nybb").replace("zip:", ""))
+    vector = (dum_dir / "nybb").with_suffix(".shp")
 
-    # download vatican city from GADM if not already exists
     if not vector.exists():
-
-        gadm_vat_link = "https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_VAT_shp.zip"
-        urlretrieve(gadm_vat_link, file)
 
         with ZipFile(file, "r") as zip_ref:
             zip_ref.extractall(dum_dir)
-
-        file.unlink()
 
     return AoiModel(alert=sw.Alert(), folder=dum_dir, gee=False, vector=vector)
