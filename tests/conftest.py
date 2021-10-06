341b84c7adf05c3588ca2dbf5adaca5a5e4f45d7
12rambau <pierrick.rambaud49@gmail.com>
2021-10-06 14:24:23 +0000

    test: use scope on decorator instead of methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,22 +9,22 @@ import sepal_ui.sepalwidgets as sw
 from sepal_ui.aoi import AoiModel
 
 
-@pytest.fixture
-def gee_ready(scope="session"):
+@pytest.fixture(scope="session")
+def gee_ready():
     """return if exporting is possible with the current GEE authentification method"""
 
     return "EE_DECRYPT_KEY" not in os.environ
 
 
-@pytest.fixture
-def root_dir(scope="session"):
+@pytest.fixture(scope="session")
+def root_dir():
     """path to the root dir of the librairy"""
 
     return Path(__file__).parents[1].absolute()
 
 
-@pytest.fixture
-def tmp_dir(scope="session"):
+@pytest.fixture(scope="session")
+def tmp_dir():
     """Creates a temporary local directory"""
 
     tmp_dir = Path.home() / "tmp" / "sepal_ui_tests"
@@ -33,43 +33,43 @@ def tmp_dir(scope="session"):
     return tmp_dir
 
 
-@pytest.fixture
-def gee_dir(scope="session"):
+@pytest.fixture(scope="session")
+def gee_dir():
     """the test dir allowed with the service account credentials"""
 
     return "projects/earthengine-legacy/assets/users/bornToBeAlive/sepal_ui_test"
 
 
-@pytest.fixture
-def asset_france(gee_dir, scope="session"):
+@pytest.fixture(scope="session")
+def asset_france(gee_dir):
     """return the france asset available in our test account"""
 
     return f"{gee_dir}/france"
 
 
-@pytest.fixture
-def asset_italy(gee_dir, scope="session"):
+@pytest.fixture(scope="session")
+def asset_italy(gee_dir):
     """return the italy asset available in our test account"""
 
     return f"{gee_dir}/italy"
 
 
-@pytest.fixture
-def asset_table_aoi(gee_dir, scope="session"):
+@pytest.fixture(scope="session")
+def asset_table_aoi(gee_dir):
     """return the aoi for the reclassify tests available in our test account"""
 
     return f"{gee_dir}/reclassify_table_aoi"
 
 
-@pytest.fixture
-def asset_image_aoi(gee_dir, scope="session"):
+@pytest.fixture(scope="session")
+def asset_image_aoi(gee_dir):
     """return the aoi for the reclassify tests available in our test account"""
 
     return f"{gee_dir}/reclassify_image_aoi"
 
 
-@pytest.fixture
-def no_name(scope="session"):
+@pytest.fixture(scope="session")
+def no_name():
     """return a no-name tuple"""
 
     return ("no_name", "#000000")
@@ -82,22 +82,22 @@ def alert():
     return sw.Alert()
 
 
-@pytest.fixture
-def readme(root_dir, scope="session"):
+@pytest.fixture(scope="session")
+def readme(root_dir):
     """return the readme file path"""
 
     return root_dir / "README.rst"
 
 
-@pytest.fixture
-def asset_description(scope="session"):
+@pytest.fixture(scope="session")
+def asset_description():
     """return a test asset name"""
 
     return "test_travis"
 
 
-@pytest.fixture
-def asset_id(asset_description, scope="session"):
+@pytest.fixture(scope="session")
+def asset_id(asset_description):
     """return a test asset id"""
 
     return f"users/bornToBeAlive/sepal_ui_test/{asset_description}"
