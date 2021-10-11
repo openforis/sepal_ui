a287dc8aaa4cad519063113a046b538f651f29d8
12rambau <pierrick.rambaud49@gmail.com>
2021-10-11 11:08:45 +0000

    fix: doc build failed

--- sepal_ui/scripts/warning.py
+++ sepal_ui/scripts/warning.py
@@ -1,7 +1,10 @@
 from deprecated.sphinx import versionadded
 
 
-@versionadded(version="2.3.1")
+@versionadded(
+    version="2.3.1",
+    reason="Added to avoid display of unrelevant warning to the end user",
+)
 class SepalWarning(Warning):
     """
     A custom warning class that will be the only one to be displayed in the Alert in voila.
