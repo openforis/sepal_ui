534596f6f1a5b45137aaf6d8796e8cab977d8b82
12rambau <pierrick.rambaud49@gmail.com>
2021-10-11 10:16:14 +0000

    fix: only display SepalWarning in Alerts

---
+++ sepal_ui/scripts/warning.py
@@ -0,0 +1,7 @@
+class SepalWarning(Warning):
+    """
+    A custom warning class that will be the only one to be displayed in the Alert in voila.
+    The other normal warning such as lib DeprecationWarning will be displayed in the notebook but hidden to the end user
+    """
+
+    pass
