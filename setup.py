916631124a803044ac9ee3cb3578480ee4295e36
12rambau <pierrick.rambaud49@gmail.com>
2022-01-11 13:06:03 +0000

    fix: only install pre-commit hooks once Fix #373

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LONG_DESCRIPTION = open("README.rst").read()
 class DevelopCmd(develop):
     def run(self):
         """overwrite run command to install pre-commit hooks in dev mode"""
-        check_call(["pre-commit", "install"])
+        check_call(["pre-commit", "install", "-t", "pre-commit", "-t", "commit-msg"])
         super().run()
 
 
