7105b800459afa5fc47461e557e52e8482a58022
12rambau <pierrick.rambaud49@gmail.com>
2022-02-15 15:01:10 +0000

    fix: avoid deprecation by reshaping dictionnary

--- sepal_ui/translator/translator.py
+++ sepal_ui/translator/translator.py
@@ -40,16 +40,16 @@ class Translator(SimpleNamespace):
         if type(json_folder) == str:
             json_folder = Path(json_folder)
 
-        # reading both the default dict
-        source_path = json_folder / {default_lan} / f"{file_pattern}.json"
-        self.default_dict = json.loads(source_path.read_text())
+        # reading the default dict
+        source_path = json_folder / default_lan / f"{file_pattern}.json"
+        self.default_dict = self.sanitize(json.loads(source_path.read_text()))
 
-        # create a composite dict replaceing all the default keys with the one availabel in the target lan
-        target_path = json_folder / {target_lan} / f"{file_pattern}.json"
+        # create a dictionary in the target language
+        target_path = json_folder / target_lan / f"{file_pattern}.json"
         self.target_dict = self.default_dict.copy()
 
         if target_path.is_file():
-            self.target_dict = json.loads(target_path.read_text())
+            self.target_dict = self.sanitize(json.loads(target_path.read_text()))
         else:
             print(f'No json file is provided for "{target_lan}", fallback to "en"')
 
@@ -88,6 +88,41 @@ class Translator(SimpleNamespace):
                     )
 
         return
+
+    @classmethod
+    def sanitize(cls, d):
+        """
+        Identify numbered dictionnaries embeded in the dict and transform them into lists
+
+        This function is an helper to prevent deprecation after the introduction of pontoon for translation.
+        The user is now force to use keys even for numbered lists. SimpleNamespace doesn't support integer indexing so this function will transform back this "numbered" dictionnary (with integer keys) into lists.
+
+        Args:
+            d (dict): the dictionnary to sanitize
+
+        Return:
+            (dict): the sanitized dictionnary
+        """
+
+        ms = d.copy()
+
+        # create generator based on input type
+        if isinstance(ms, dict):
+            gen = ms.items()
+        elif isinstance(ms, list):
+            gen = enumerate(ms)
+
+        # loop into the keys of the dict modify them
+        for k, v in gen:
+            if isinstance(v, dict):
+                tmp = v
+                if all([k.isnumeric() for k in tmp]):
+                    tmp = list(tmp.values())
+                ms[k] = cls.sanitize(tmp)
+            else:
+                ms[k] = v
+
+        return ms
 
     def _update(self, d, u):
         """
--- tests/test_Translator.py
+++ tests/test_Translator.py
@@ -2,35 +2,37 @@ import pytest
 from pathlib import Path
 import io
 from contextlib import redirect_stdout
+import shutil
+import json
 
 from sepal_ui.translator import Translator
 
 
 class TestTranslator:
-    def test_init(self):
+    def test_init(self, translation_folder):
 
         # assert that the test key exist in fr
         target_lan = "fr"
-        translator = Translator(self._get_message_json_folder(), target_lan)
+        translator = Translator(translation_folder, target_lan)
 
         assert translator.test_key == "Clef de test"
 
         # assert that the the code work if the path is a str
         target_lan = "fr"
-        translator = Translator(str(self._get_message_json_folder()), target_lan)
+        translator = Translator(translation_folder, target_lan)
         assert translator.test_key == "Clef de test"
 
         # assert that the test does not exist in es and we fallback to en
         target_lan = "es"
 
-        translator = Translator(self._get_message_json_folder(), target_lan)
+        translator = Translator(translation_folder, target_lan)
         assert translator.test_key == "Test key"
 
         # assert that using a non existing lang lead to a warning
         target_lan = "it"
         f = io.StringIO()
         with redirect_stdout(f):
-            translator = Translator(self._get_message_json_folder(), target_lan)
+            translator = Translator(translation_folder, target_lan)
         assert f.getvalue() == 'No json file is provided for "it", fallback to "en"\n'
 
         return
@@ -46,10 +48,33 @@ class TestTranslator:
 
         return
 
-    def _get_message_json_folder(self):
+    @pytest.fixture(scope="class")
+    def translation_folder(self):
+        """
+        Generate a fully qualified trnaslation folder with limited keys in en, fr and spanish.
+        Cannot use the temfile lib as we need the directory to appear in the tree
+        """
 
-        # use the json folder of the lib
-        home = Path(__file__).parent.parent.absolute()
-        path = home / "sepal_ui" / "message"
+        # set up the appropriate keys for each language
+        keys = {
+            "en": {"a_key": "A key", "test_key": "Test key"},
+            "fr": {"a_key": "Une clef", "test_key": "Clef de test"},
+            "es": {"a_key": "Una llave"},
+        }
 
-        return path
+        # generate the tmp_dir in the test directory
+        tmp_dir = Path(__file__).parent() / "data" / "messages"
+        tmp_dir.mkdir(exist_ok=True, parents=True)
+
+        # create the translation files
+        for lan, d in keys.items():
+            folder = tmp_dir / lan
+            folder.mkdir()
+            (folder / "locale.json").write_text(json.dumps(d, indent=2))
+
+        yield tmp_dir
+
+        # flush everything
+        shutil.rmtree(tmp_dir)
+
+        return
