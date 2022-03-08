4366906e701229538aa2a6f93467c216827cae20
12rambau <pierrick.rambaud49@gmail.com>
2022-03-08 15:47:59 +0000

    docs: typo in SepalMap arg name

--- sepal_ui/mapping/mapping.py
+++ sepal_ui/mapping/mapping.py
@@ -57,7 +57,7 @@ class SepalMap(geemap.Map):
         basemaps ['str']: the basemaps used as background in the map. If multiple selection, they will be displayed as layers.
         dc (bool, optional): wether or not the drawing control should be displayed. default to false
         vinspector (bool, optional): Add value inspector to map, useful to inspect pixel values. default to false
-        ee (bool, optional): wether or not to use the ee binding. If False none of the earthengine display fonctionalities can be used. default to True
+        gee (bool, optional): wether or not to use the ee binding. If False none of the earthengine display fonctionalities can be used. default to True
         kwargs (optional): any parameter from a geemap.Map. if set, 'ee_initialize' will be overwritten.
     """
 
