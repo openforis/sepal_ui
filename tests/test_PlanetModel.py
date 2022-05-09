b431f9d0b97474c9424fdd0026ed0da4d2ae2d53
dfguerrerom <dfgm2006@gmail.com>
2022-05-09 16:03:33 +0000

    test: test credentials str

--- tests/test_PlanetModel.py
+++ tests/test_PlanetModel.py
@@ -15,7 +15,8 @@ class TestPlanetModel:
     @pytest.fixture
     def cred(self):
 
-        credentials = json.loads(os.getenv("PLANET_API_CREDENTIALS"))
+        # credentials = json.loads(os.getenv("PLANET_API_CREDENTIALS"))
+        credentials = json.loads('{"username": "s@gmail.com", "password": "s"}')
 
         return tuple(credentials.values())
 
