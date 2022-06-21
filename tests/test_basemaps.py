fc2e931e2f8e83e79b06a70ac78354d14fd0e1c9
Rambaud Pierrick <12rambau@users.noreply.github.com>
2022-06-21 10:44:58 +0200

    test: adapt number of xyz layers

--- tests/test_basemaps.py
+++ tests/test_basemaps.py
@@ -5,13 +5,13 @@ from ipyleaflet import TileLayer
 class TestBasemaps:
     def test_get_xyz_dict(self):
 
-        assert len(sm.basemaps.get_xyz_dict()) == 123
+        assert len(sm.basemaps.get_xyz_dict()) == 126
 
         return
 
     def test_xyz_to_leaflet(self):
 
-        assert len(sm.basemaps.xyz_to_leaflet()) == 128
+        assert len(sm.basemaps.xyz_to_leaflet()) == 131
         for tile in sm.basemaps.xyz_to_leaflet().values():
             assert isinstance(tile, TileLayer)
 
