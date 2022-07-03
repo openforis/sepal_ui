949e4ee1218ff17e4250aaca2ef56fc45e398439
12rambau <pierrick.rambaud49@gmail.com>
2022-07-03 10:32:56 +0000

    fix: use txt in mapbtn Fix #510

--- sepal_ui/mapping/map_btn.py
+++ sepal_ui/mapping/map_btn.py
@@ -7,20 +7,27 @@ from sepal_ui.frontend.styles import map_btn_style
 class MapBtn(v.Btn, sw.SepalWidget):
     """
     Btn specifically design to be displayed on a map. It matches all the characteristics of
-    the classic leaflet btn but as they are from ipyvuetify we can use them in combination with Menu to produce on-the-map. The MapBtn is responsive to theme changes.
-    Tiles. It only accept icon as children as the space is very limited.
+    the classic leaflet btn but as they are from ipyvuetify we can use them in combination with Menu to produce on-the-map tiles.
+    The MapBtn is responsive to theme changes. It only accept icon or 3 letters as children as the space is very limited.
+    If both ``logo`` and ``msg`` are filled, msg will have the priority.
 
     Args:
-        logo (str): a fas/mdi fully qualified name
+        logo (str, optional): a fas/mdi fully qualified name.
+        name (str, optional): the name of the btn using 3 letters max.
     """
 
     logo = None
-    "(sw.Icon): a sw.Icon"
+    "(sw.Icon|str): the content of the btn"
 
-    def __init__(self, logo, **kwargs):
+    def __init__(self, logo=None, msg=None, **kwargs):
 
         # create the icon
-        self.logo = sw.Icon(small=True, children=[logo])
+        if msg is not None:
+            self.logo = msg[: min(3, len(msg))].upper()
+        elif logo is not None:
+            self.logo = sw.Icon(small=True, children=[logo])
+        else:
+            raise ValueError("at least one of logo or msg need to be set")
 
         # some parameters are overloaded to match the map requirements
         kwargs["color"] = "text-color"
--- tests/test_MapBtn.py
+++ tests/test_MapBtn.py
@@ -1,11 +1,35 @@
+import pytest
+
 from sepal_ui import mapping as sm
+from sepal_ui import sepalwidgets as sw
 
 
 class TestMapBtn:
-    def test_map_btn(self):
+    def test_init(self):
 
+        # check that the call with no kwargs still triger icon for no-regression
         map_btn = sm.MapBtn("fas fa-folder")
-
         assert isinstance(map_btn, sm.MapBtn)
+        assert isinstance(map_btn.children[0], sw.Icon)
+        assert map_btn.children[0].children[0] == "fas fa-folder"
+
+        map_btn = sm.MapBtn(msg="to")
+        assert isinstance(map_btn.children[0], str)
+        assert map_btn.children[0] == "TO"
+
+        map_btn = sm.MapBtn(msg="toto")
+        assert isinstance(map_btn.children[0], str)
+        assert map_btn.children[0] == "TOT"
+
+        # check that text has priority
+        map_btn = sm.MapBtn(logo="fas fa-folder", msg="toto")
+        assert isinstance(map_btn.children[0], str)
+        assert map_btn.children[0] == "TOT"
+
+        # check error is raised when nothing is set
+        with pytest.raises(
+            ValueError, match="at least one of logo or msg need to be set"
+        ):
+            map_btn = sm.MapBtn()
 
         return
