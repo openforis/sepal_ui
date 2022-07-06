df9ba15cb25feeec0fcd6f2648d2422d5c37d36e
12rambau <pierrick.rambaud49@gmail.com>
2022-07-06 08:51:55 +0000

    refactor: edit style file structure

--- sepal_ui/frontend/styles.py
+++ sepal_ui/frontend/styles.py
@@ -9,6 +9,26 @@ import ipyvuetify as v
 from sepal_ui import config
 import sepal_ui.scripts.utils as su
 
+################################################################################
+# access the folders where style information is stored (layers, widgets)
+#
+
+# the colors are set using tables as follow.
+# 1 (True): dark theme
+# 0 (false): light theme
+JSON_DIR = Path(__file__).parent / "json"
+"pathlib.Path: the path to the json style folder"
+
+CSS_DIR = Path(__file__).parent / "css"
+"pathlib.Path: the path to the css style folder"
+
+JS_DIR = Path(__file__).parent / "js"
+"pathlib.Path: the path to the js style folder"
+
+################################################################################
+# define all the colors taht we want to use in the theme
+#
+
 DARK_THEME = {
     "primary": "#b3842e",
     "accent": "#a1458e",
@@ -39,20 +59,13 @@ LIGHT_THEME = {
 }
 "dict: colors used for the light theme"
 
-# the colors are set using tables as follow.
-# 1 (True): dark theme
-# 0 (false): light theme
-JSON_DIR = Path(__file__).parent / "json"
-"pathlib.Path: the path to the json style folder"
-
-CSS_DIR = Path(__file__).parent / "css"
-"pathlib.Path: the path to the css style folder"
-
-JS_DIR = Path(__file__).parent / "js"
-"pathlib.Path: the path to the js style folder"
 
 if not DARK_THEME.keys() == LIGHT_THEME.keys():
     raise Exception("Both dictionaries has to have the same color names")
+
+################################################################################
+# define classes and method to make the application resonsive
+#
 
 
 def get_theme():
@@ -71,7 +84,6 @@ class SepalColor(HasTraits, SimpleNamespace):
     with a magic method to display theme.
     """
 
-    # Initialize with the current theme
     _dark_theme = Bool(True if get_theme() == "dark" else False).tag(sync=True)
     "bool: whether to use dark theme or not. By changing this value, the theme value will be stored in the conf file. Is only intended to be accessed in development mode."
 
