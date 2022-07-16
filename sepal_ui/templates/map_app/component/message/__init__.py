6db50b38ac35f438f5cb17b5584b9e18ed932058
12rambau <pierrick.rambaud49@gmail.com>
2022-07-16 08:19:35 +0000

    fix: add the map_app template

---
+++ sepal_ui/templates/map_app/component/message/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+from sepal_ui.translator import Translator
+
+# create a translator object
+cm = Translator(Path(__file__).parent)
---
+++ sepal_ui/templates/map_app/component/parameter/__init__.py
@@ -0,0 +1,5 @@
+# if you only have few widgets, a module is not necessary and you can simply use a parameter.py file
+# in a big module with lot of custom parmeters, it can make sense to split things in separate files for the sake of maintenance
+
+# if you use a module import all the functions here to only have 1 call to make
+from .directory import *
