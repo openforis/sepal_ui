3ce95bf71b1acd325486a49cda7549ade6ed61c3
12rambau <pierrick.rambaud49@gmail.com>
2022-09-01 07:26:57 +0000

    bump: version 2.10.3 → 2.11.0

--- sepal_ui/__init__.py
+++ sepal_ui/__init__.py
@@ -5,7 +5,7 @@ from sepal_ui.frontend.styles import SepalColor, get_theme
 
 __author__ = """Pierrick Rambaud"""
 __email__ = "pierrick.rambaud49@gmail.com"
-__version__ = "2.10.3"
+__version__ = "2.11.0"
 
 color = SepalColor()
 'color: the colors of sepal. members are in the following list: "main, darker, bg, primary, accent, secondary, success, info, warning, error, menu". They will render according to the selected theme.'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from subprocess import check_call
 from setuptools import setup
 from setuptools.command.develop import develop
 
-version = "2.10.3"
+version = "2.11.0"
 
 DESCRIPTION = "Wrapper for ipyvuetify widgets to unify the display of voila dashboards in SEPAL platform"
 LONG_DESCRIPTION = open("README.rst").read()
