ad06df933f4acb28b214008ff6b164ffe99853b9
Rambaud Pierrick <12rambau@users.noreply.github.com>
2022-12-11 18:01:05 +0100

    build: simplify planet call

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup_params = {
         "cryptography",
         "python-box",
         "xyzservices",
-        "planet==2.0a2",  # this is a prerelease
+        "planet>=2.0",
         "pyyaml",
         "dask",
         "tqdm",
