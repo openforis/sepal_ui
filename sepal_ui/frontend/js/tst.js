8c0666c495367a78fd9351f964b68971bae30bb2
dfguerrerom <dfgm2006@gmail.com>
2024-03-27 20:44:36 +0100

    test

--- sepal_ui/frontend/js/tst.js
+++ sepal_ui/frontend/js/tst.js
@@ -1,13 +1,25 @@
-/* set a selected map to fullscreen */
-var i = 0;
-const wait_unitl_element_appear = setInterval(() => {
-  if (i > 50) {
-    const elements = document.querySelectorAll(
-      ".vuetify-styles div.v-application--wrap"
-    );
-    const lastElement = elements[elements.length - 1];
-    lastElement.style.minHeight = "100vh";
-  } else {
-    i++;
-  }
-}, 100);
+// console.log('asdfasdf');
+            
+// // Select the .v-application-wrapper element
+// var wrappers = document.querySelectorAll('.vuetify-styles div.v-application--wrap');
+
+// //  print the wrappers
+// console.log(wrappers);
+
+// // Loop through each wrapper to check for a div with id="app"
+// wrappers.forEach(function(wrapper) {
+//   var appDiv = wrapper.querySelector('#app');
+
+//   // If an appDiv is found within this wrapper
+//   if(appDiv) {
+//       // Check if the special-style class has already been applied to the wrapper
+//       if(wrapper.classList.contains('special-style')) {
+//       console.log('special-style already applied to a wrapper containing #app');
+//       // Perform any additional actions knowing the class is already applied
+//       } else {
+//       // If the special-style class hasn't been applied yet, you can apply it now
+//       wrapper.classList.add('special-style');
+//       console.log('special-style applied to the wrapper containing #app');
+//       }
+//   }
+// });
