a332bfb7ec620d99f28fb78c178f07c682ea45e8
dfguerrerom <dfgm2006@gmail.com>
2024-10-20 19:38:18 +0200

    bump: version 2.20.2 → 2.20.3

--- sepal_ui/__init__.py
+++ sepal_ui/__init__.py
@@ -10,7 +10,7 @@ from sepal_ui.frontend.styles import get_theme as get_theme
 
 __author__ = """Pierrick Rambaud"""
 __email__ = "pierrick.rambaud49@gmail.com"
-__version__ = "2.20.2"
+__version__ = "2.20.3"
 
 color = SepalColor()
 'color: the colors of sepal. members are in the following list: "main, darker, bg, primary, accent, secondary, success, info, warning, error, menu". They will render according to the selected theme.'
