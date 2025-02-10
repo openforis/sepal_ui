c5576e9bf023baea319c47207b3f3478c98cc99e
dfguerrerom <dfgm2006@gmail.com>
2025-02-10 09:28:24 -0500

    feat: use vue template to trigger resize events in drawers

--- sepal_ui/sepalwidgets/app.py
+++ sepal_ui/sepalwidgets/app.py
@@ -14,17 +14,27 @@ Example:
 from datetime import datetime
 from itertools import cycle
 from pathlib import Path
-from typing import List, Optional
+from typing import Optional
 
 import ipyvuetify as v
 import pandas as pd
-import traitlets as t
 from deprecated.sphinx import versionadded, versionchanged
-from ipywidgets import jsdlink
-from traitlets import link, observe
+from ipywidgets import DOMWidget, jsdlink
+from ipywidgets.widgets.widget import widget_serialization
+from traitlets import (
+    Any,
+    Bool,
+    Dict,
+    Instance,
+    Int,
+    List,
+    Unicode,
+    Union,
+    link,
+    observe,
+)
 from typing_extensions import Self
 
-from sepal_ui.frontend.resize_trigger import ResizeTrigger, rt
 from sepal_ui.frontend.styles import get_theme
 from sepal_ui.message import ms
 from sepal_ui.model import Model
@@ -280,11 +290,16 @@ class AppBar(v.AppBar, SepalWidget):
         return self
 
 
-class DrawerItem(v.ListItem, SepalWidget):
-    rt: Optional[ResizeTrigger] = None
-    "The trigger to resize maps and other javascript object when jumping from a tile to another"
+class DrawerItem(v.VuetifyTemplate):
 
-    alert: t.Bool = t.Bool(False).tag(sync=True)
+    href = Union([Unicode(), Dict()], default_value=None, allow_none=True).tag(sync=True)
+    input_value = Any(None, allow_none=True).tag(sync=True)
+    link = Bool(None, allow_none=True).tag(sync=True)
+    target = Unicode(None, allow_none=True).tag(sync=True)
+    resize = Int().tag(sync=True)
+    _metadata = Dict(default_value=None, allow_none=True).tag(sync=True)
+
+    alert = Bool().tag(sync=True)
     "Trait to control visibility of an alert in the drawer item"
 
     alert_badge: Optional[v.ListItemAction] = None
@@ -292,6 +307,9 @@ class DrawerItem(v.ListItem, SepalWidget):
 
     tiles: Optional[List[v.Card]] = None
     "the cards of the application"
+
+    children = List(Union([Instance(DOMWidget), Unicode()])).tag(sync=True, **widget_serialization)
+    template_file = Unicode(str(Path(__file__).parent / "vue/DrawerItem.vue")).tag(sync=True)
 
     def __init__(
         self,
@@ -317,9 +335,6 @@ class DrawerItem(v.ListItem, SepalWidget):
             bind_var: required when model is selected. Trait to link with 'alert' self trait parameter
             kwargs (optional): any parameter from a v.ListItem. If set, '_metadata', 'target', 'link' and 'children' will be overwritten.
         """
-        # set the resizetrigger
-        self.rt = rt
-
         icon = icon if icon else "fa-regular fa-folder"
 
         children = [
@@ -387,21 +402,14 @@ class DrawerItem(v.ListItem, SepalWidget):
             tiles: the list of all the available tiles in the app
         """
         self.tiles = tiles
-        self.on_event("click", self._on_click)
 
-        return self
-
-    def _on_click(self, *args) -> Self:
-
-        if self.callback:
-            self.callback()
+    def vue_on_click_python(self, data=None) -> Self:
+        """Display the appropriate tiles when the item is clicked."""
+        self.resize += 1
 
         for tile in self.tiles:
             show = self._metadata["card_id"] == tile._metadata["mount_id"]
             tile.viz = show
-
-        # trigger the resize event
-        self.rt.resize()
 
         # change the current item status
         self.input_value = True
