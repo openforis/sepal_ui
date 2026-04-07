974840df67b0747062269eb8e5d69a67432bf0a0
dfguerrerom <dfgm2006@gmail.com>
2026-04-07 17:49:02 +0200

    feat: add input selector Solara components
    AssetSelectComponent (GEE asset picker), PointsSelectorComponent (CSV),
    and VectorSelectorComponent (vector files). File I/O uses use_task +
    asyncio.to_thread to avoid blocking the render thread.

---
+++ pysepal/solara/components/inputs/__init__.py
@@ -0,0 +1,13 @@
+"""Solara input components for pysepal."""
+
+from .asset_select import AssetSelectComponent
+from .file_input import FileInputComponent
+from .point_selector import PointsSelectorComponent
+from .vector_selector import VectorSelectorComponent
+
+__all__ = [
+    "FileInputComponent",
+    "AssetSelectComponent",
+    "PointsSelectorComponent",
+    "VectorSelectorComponent",
+]
